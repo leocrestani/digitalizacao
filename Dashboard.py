39bc6bd4ecc6dc8e89a6b9451cb8bde1a4cd533b
Leonardo Crestani <leogolincrestani@hotmail.com>
2025-06-14 18:12:34 -0300

    Implementar gráfico de anomalias

--- Dashboard.py
+++ Dashboard.py
@@ -10,7 +10,7 @@ st.title("Dashboard de Visão de Máquinas")
 
 data = pd.read_csv('smart_manufacturing_data.csv')
 
-aba1, aba2, aba3 = st.tabs(["Visão Geral", "Status das máquinas", "Correlação entre Features"])
+aba1, aba2, aba3, aba4 = st.tabs(["Visão Geral", "Status das máquinas", "Correlação entre Features", "Análise de Anomalias"])
 
 with aba1:
     st.header("Visão Geral")
@@ -51,10 +51,18 @@ with aba3:
     data['timestamp'] = pd.to_datetime(data['timestamp'])
     data['timestamp'] = data['timestamp'].astype(int) / 10**9
     features = ["timestamp", "temperature", "vibration", "humidity", "pressure", "energy_consumption", "predicted_remaining_life", "downtime_risk"]
-    data = data[features]
-    data = data.dropna() 
-    corr = data.corr(method='pearson')
+    data_feature = data[features]
+    data_feature = data_feature.dropna() 
+    corr = data_feature.corr(method='pearson')
     fig_corr = px.imshow(corr, text_auto=True, aspect="auto", color_continuous_scale='RdBu_r',
                          title="Correlação entre Features")
     fig_corr.update_layout(xaxis_title="Features", yaxis_title="Features")
     st.plotly_chart(fig_corr, use_container_width=True)
+
+with aba4:
+    st.header("Análise de Anomalias")
+    anomalies = data[data['anomaly_flag'] == "Yes"]
+    fig_anomalies = px.scatter(anomalies, x='timestamp', y='temperature', color='machine',
+                                title="Anomalias Detectadas por Máquina",
+                                labels={"timestamp": "Timestamp", "temperature": "Temperatura", "machine": "Máquina"})
+    st.plotly_chart(fig_anomalies, use_container_width=True)
